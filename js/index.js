3149ad97c5ed2b404a301203664a6570a580b038
Dmitriy Stychuk <fenderchord@gmail.com>
2022-07-26 22:13:47 +0300

    Updates

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+let length = document.querySelector('#input1');
+let width = document.querySelector('#input2');
+let weight = document.querySelector('#input3');
+let button = document.querySelector('#button');
+
+function resault() {
+  alert(Number(length.value) + Number(width.value));
+}
+
+button.addEventListener('click', resault);
