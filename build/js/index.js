1f5057637e9a9b04688690ac8904ce08a5d53b9e
Dmitriy Stychuk <fenderchord@gmail.com>
2022-07-30 21:15:06 +0300

    add tags for culc

--- build/js/index.js
+++ build/js/index.js
@@ -3,8 +3,10 @@ let width = document.querySelector('#input2');
 let weight = document.querySelector('#input3');
 let button = document.querySelector('#button');
 
-function resault() {
-  alert(Number(length.value) + Number(width.value));
-}
+button.addEventListener('click', function () {
+  input10.innerHTML =
+    (Number(length.value) * Number(width.value) * Number(weight.value) * 7.85) /
+    1000000;
 
-button.addEventListener('click', resault);
+  input11.innerHTML = (Number(length.value) * Number(width.value)) / 1000000;
+});
