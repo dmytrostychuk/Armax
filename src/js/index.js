49a38ca052d019c9b4b21632b5d68a9b6708cccd
Dmitriy Stychuk <fenderchord@gmail.com>
2022-08-27 16:43:27 +0300

    add tags

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,43 @@
-let length = document.querySelector('#input1');
-let width = document.querySelector('#input2');
-let weight = document.querySelector('#input3');
-let button = document.querySelector('#button');
+let calcSheetBtn = document.querySelector('.calc__sheet-btn');
+let calcProfilePipeBtn = document.querySelector('.calc__profile-pipe-btn');
+let calcPipeBtn = document.querySelector('.calc__pipe-btn');
 
-button.addEventListener('click', function () {
-  input10.innerHTML =
+let calcSheet = document.querySelector('.calc__sheet');
+let calcProfilePipe = document.querySelector('.calc__profile-pipe');
+let calcPipe = document.querySelector('.calc__pipe');
+
+calcSheetBtn.addEventListener('click', function () {
+  if (calcSheetBtn) {
+    calcSheet.classList.toggle('active');
+    calcProfilePipe.classList.toggle('disabled');
+  } else {
+  }
+});
+
+calcProfilePipeBtn.addEventListener('click', function () {
+  if (calcProfilePipeBtn) {
+    calcProfilePipe.classList.toggle('active');
+    calcProfilePipe.classList.remove('disabled');
+    calcSheet.classList.remove('acive');
+  } else {
+  }
+});
+
+calcPipeBtn.addEventListener('click', function () {
+  if (calcPipeBtn) {
+    calcPipe.classList.toggle('active');
+  }
+});
+
+let length = document.querySelector('#length');
+let width = document.querySelector('#width');
+let weight = document.querySelector('#weight');
+let sheetBtn = document.querySelector('#calc__sheet-button');
+
+sheetBtn.addEventListener('click', function () {
+  mass.innerHTML =
     (Number(length.value) * Number(width.value) * Number(weight.value) * 7.85) /
     1000000;
 
-  input11.innerHTML = (Number(length.value) * Number(width.value)) / 1000000;
+  area.innerHTML = (Number(length.value) * Number(width.value)) / 1000000;
 });
