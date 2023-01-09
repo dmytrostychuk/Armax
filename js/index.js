085201de68bf24a9589d5d6cf01c5040cf5f8e74
Dmitriy Stychuk <fenderchord@gmail.com>
2023-01-09 12:58:19 +0200

    Updates

--- js/index.js
+++ js/index.js
@@ -15,47 +15,6 @@ if (burgerBtn) {
   });
 }
 
-const swiper = new Swiper('.swiper', {
-  slidesPerView: 2,
-  infinity: true,
-  spaceBetween: 10,
-  // autoplay: {
-  //   delay: 800,
-  //   disableOnInteraction: false,
-  // },
-  speed: 800,
-  breakpoints: {
-    480: {
-      spaceBetween: 30,
-      slidesPerView: 2,
-    },
-    768: {
-      spaceBetween: 50,
-      slidesPerView: 3,
-    },
-    1024: {
-      spaceBetween: 60,
-      slidesPerView: 4,
-    },
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  on: {
-    init() {
-      this.el.addEventListener('mouseenter', () => {
-        this.autoplay.stop();
-      });
-
-      this.el.addEventListener('mouseleave', () => {
-        this.autoplay.start();
-      });
-    },
-  },
-});
-
 //modal
 
 var modal = document.querySelector('.modal');
