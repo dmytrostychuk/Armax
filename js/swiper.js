4e7c62b0c879b3a7939b15742077f986869340c7
Dmitriy Stychuk <fenderchord@gmail.com>
2023-04-17 19:27:33 +0300

    Updates

--- js/swiper.js
+++ js/swiper.js
@@ -45,7 +45,7 @@ AOS.init();
 // below listed default settings
 AOS.init({
   // Global settings:
-  disable: mobile, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  disable: 'false', // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
   startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
   initClassName: 'aos-init', // class applied after initialization
   animatedClassName: 'aos-animate', // class applied on animation
@@ -57,7 +57,7 @@ AOS.init({
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   offset: 100, // offset (in px) from the original trigger point
   delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 800, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
   easing: 'ease', // default easing for AOS animations
   once: true, // whether animation should happen only once - while scrolling down
   mirror: false, // whether elements should animate out while scrolling past them
