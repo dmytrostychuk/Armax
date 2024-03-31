b70b4e56e2964c56d7cf6a49fb1b3a2b1578720d
Dmitriy Stychuk <fenderchord@gmail.com>
2024-04-01 00:07:06 +0300

    Updates

--- js/index.js
+++ js/index.js
@@ -25,30 +25,6 @@ if (burgerBtn) {
   });
 }
 
-const calcFormButtons = document.querySelectorAll('.calc-form-button-js');
-const calcForms = document.querySelectorAll('.calc-form-js');
-
-function resetCalcForms() {
-  for (let i = 0; i < calcForms.length; i++) {
-    calcForms[i].classList.remove('active');
-  }
-}
-
-function addFormActiveClass(type) {
-  Array.from(calcForms)
-    .filter((el) => el.dataset.type === type)[0]
-    .classList.add('active');
-}
-
-function handleFormButtonClick(e) {
-  resetCalcForms();
-  addFormActiveClass(e.target.dataset.type);
-}
-
-for (let i = 0; i < calcFormButtons.length; i++) {
-  calcFormButtons[i].addEventListener('click', handleFormButtonClick);
-}
-
 document.addEventListener('DOMContentLoaded', function () {
   var scrollToFormButton = document.querySelector('.scroll-to-form');
 
@@ -84,28 +60,6 @@ window.addEventListener('load', toggleScrollButton);
 window.addEventListener('resize', toggleScrollButton);
 // Прослуховування прокрутки сторінки
 window.addEventListener('scroll', toggleScrollButton);
-
-// var length = document.querySelector('.length');
-// var width = document.querySelector('.width');
-// var weight = document.querySelector('.weight');
-// let sheetBtn = document.querySelector('.calc__sheet-button');
-// let pipeBtn = document.querySelector('.calc__pipe-button');
-
-// sheetBtn.addEventListener('click', function (e) {
-//   e.preventDefault();
-//   mass.innerHTML = (
-//     (Number(length.value) * Number(width.value) * Number(weight.value) * 7.85) /
-//     1000000
-//   ).toFixed(2);
-//   area.innerHTML = (
-//     (Number(length.value) * Number(width.value)) /
-//     1000000
-//   ).toFixed(2);
-// });
-
-// pipeBtn.addEventListener('click', function (e) {
-//   e.preventDefault();
-// });
 
 document.addEventListener('DOMContentLoaded', function () {
   const form = document.getElementById('form');
