c626e9c216f5108240be99b9541d94f256dc07b8
Dmitriy Stychuk <fenderchord@gmail.com>
2024-03-31 11:53:54 +0300

    Updates

--- js/index.js
+++ js/index.js
@@ -1,113 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const form = document.getElementById('form');
-  form.addEventListener('submit', formSend);
-
-  async function formSend(e) {
-    e.preventDefault();
-
-    let error = formValidate(form);
-
-    let formData = new FormData(form);
-    formData.append('image', formImage.files[0]);
-
-    if (error === 0) {
-      form.classList.add('_sending');
-      try {
-        let response = await fetch('sendmail.php', {
-          method: 'POST',
-          body: formData,
-        });
-        if (response.status >= 200 && response.status < 300) {
-          let result = await response.json();
-          alert(result.message);
-          formPreview.innerHTML = '';
-          form.reset();
-        } else {
-          throw new Error('Помилка: ' + response.status);
-        }
-      } catch (err) {
-        console.error('Помилка відправлення форми:', err);
-        alert(
-          'Помилка відправлення форми. Будь ласка, спробуйте ще раз пізніше.'
-        );
-      } finally {
-        form.classList.remove('_sending');
-      }
-    } else {
-      alert("Заповніть обов'язкові поля");
-    }
-  }
-
-  function formValidate(form) {
-    let error = 0;
-    let formReq = document.querySelectorAll('._req');
-
-    formReq.forEach((input) => {
-      formRemoveError(input);
-
-      if (input.classList.contains('_email')) {
-        if (emailTest(input)) {
-          formAddError(input);
-          error++;
-        }
-      } else if (input.type === 'checkbox' && !input.checked) {
-        formAddError(input);
-        error++;
-      } else {
-        if (!input.value.trim()) {
-          formAddError(input);
-          error++;
-        }
-      }
-    });
-
-    return error;
-  }
-
-  function formAddError(input) {
-    input.parentElement.classList.add('_error');
-    input.classList.add('_error');
-  }
-
-  function formRemoveError(input) {
-    input.parentElement.classList.remove('_error');
-    input.classList.remove('_error');
-  }
-
-  function emailTest(input) {
-    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-  }
-
-  const formImage = document.getElementById('formImage');
-  const formPreview = document.getElementById('formPreview');
-
-  formImage.addEventListener('change', () => {
-    uploadFile(formImage.files[0]);
-  });
-
-  function uploadFile(file) {
-    if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)) {
-      alert('Дозволяються лише зображення.');
-      formImage.value = '';
-      return;
-    }
-    if (file.size > 2 * 1024 * 1024) {
-      alert('Файл повинен бути меншим за 2 МБ.');
-      return;
-    }
-
-    var reader = new FileReader();
-    reader.onload = function (e) {
-      formPreview.innerHTML = `<img src="${e.target.result}" alt="Фото">`;
-    };
-    reader.onerror = function (e) {
-      console.error('Помилка завантаження файлу:', e);
-      alert('Помилка завантаження файлу.');
-    };
-    reader.readAsDataURL(file);
-  }
-});
-
 // ========================================================================
 
 let burgerBtn = document.querySelector('.burger-btn');
@@ -216,3 +106,117 @@ window.addEventListener('scroll', toggleScrollButton);
 // pipeBtn.addEventListener('click', function (e) {
 //   e.preventDefault();
 // });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('form');
+  form.addEventListener('submit', formSend);
+
+  async function formSend(e) {
+    e.preventDefault();
+
+    let error = formValidate(form);
+
+    let formData = new FormData(form);
+    formData.append('image', formImage.files[0]);
+
+    if (error === 0) {
+      form.classList.add('_sending');
+      try {
+        let response = await fetch('sendmail.php', {
+          method: 'POST',
+          body: formData,
+        });
+        if (response.status >= 200 && response.status < 300) {
+          let result = await response.json();
+          alert(result.message);
+          formPreview.innerHTML = '';
+          form.reset();
+        } else {
+          throw new Error('Помилка: ' + response.status);
+        }
+      } catch (err) {
+        console.error('Помилка відправлення форми:', err);
+        alert(
+          'Помилка відправлення форми. Будь ласка, спробуйте ще раз пізніше.'
+        );
+      } finally {
+        form.classList.remove('_sending');
+      }
+    } else {
+      alert("Заповніть обов'язкові поля");
+    }
+  }
+
+  function formValidate(form) {
+    let error = 0;
+    let formReq = document.querySelectorAll('._req');
+
+    formReq.forEach((input) => {
+      formRemoveError(input);
+
+      if (input.classList.contains('_email')) {
+        if (emailTest(input)) {
+          formAddError(input);
+          error++;
+        }
+      } else if (input.type === 'checkbox' && !input.checked) {
+        formAddError(input);
+        error++;
+      } else {
+        if (!input.value.trim()) {
+          formAddError(input);
+          error++;
+        }
+        if (input.classList.contains('input-number') && !input.validity.valid) {
+          formAddError(input);
+          error++;
+        }
+      }
+    });
+
+    return error;
+  }
+
+  function formAddError(input) {
+    input.parentElement.classList.add('_error');
+    input.classList.add('_error');
+  }
+
+  function formRemoveError(input) {
+    input.parentElement.classList.remove('_error');
+    input.classList.remove('_error');
+  }
+
+  function emailTest(input) {
+    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+  }
+
+  const formImage = document.getElementById('formImage');
+  const formPreview = document.getElementById('formPreview');
+
+  formImage.addEventListener('change', () => {
+    uploadFile(formImage.files[0]);
+  });
+
+  function uploadFile(file) {
+    if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)) {
+      alert('Дозволяються лише зображення.');
+      formImage.value = '';
+      return;
+    }
+    if (file.size > 2 * 1024 * 1024) {
+      alert('Файл повинен бути меншим за 2 МБ.');
+      return;
+    }
+
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      formPreview.innerHTML = `<img src="${e.target.result}" alt="Фото">`;
+    };
+    reader.onerror = function (e) {
+      console.error('Помилка завантаження файлу:', e);
+      alert('Помилка завантаження файлу.');
+    };
+    reader.readAsDataURL(file);
+  }
+});
